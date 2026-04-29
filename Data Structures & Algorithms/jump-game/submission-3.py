e50d18dc9ef08f03aaf4b8523c01c162069b03b3
arthfs <78196956+arthfs@users.noreply.github.com>
2026-04-29 14:06:52 -0400

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        dp = [0]* (n+1)
+        dp[0] = nums[0]
+        last = dp[0] if dp[0] >0 else -1
+        for i in range(1, n):
+            if last >= i:
+                dp[i] = max( [dp[last], i + nums[i] ])
+                if dp[i] > n-1:
+                    dp[i] = n-1
+
+            last = max([dp[i], last])
+   
+        return dp[-2] >= n-1
