f916afcea941a16bb705eadbbf2fd3448bd6e70e
arthfs <78196956+arthfs@users.noreply.github.com>
2026-04-29 18:09:24 -0400

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        ref = []
+        intervals = sorted(intervals , key = lambda x: (x[0], x[1]))
+        ref.append(intervals.pop(0))
+
+        for i in intervals:
+            if i[0] <= ref[-1] [1]:
+                ref[-1] = [min ([i[0], ref[-1] [0]]), max([i[1], ref[-1] [1]])]
+            else:
+                ref.append(i)
+
+        return ref
