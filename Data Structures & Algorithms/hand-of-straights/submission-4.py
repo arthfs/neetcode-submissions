b078d29827bf7fbe9a2381a76e669255319b5ae3
arthfs <78196956+arthfs@users.noreply.github.com>
2026-04-30 08:53:17 -0400

    Add: hand-of-straights - submission-4

---
+++ Data Structures & Algorithms/hand-of-straights/submission-4.py
@@ -0,0 +1,33 @@
+from collections import Counter
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        ref = Counter(hand)
+        
+        temp = []
+        queue = sorted(list(ref.keys()))
+
+        while queue!= [] :
+            if temp == []:
+                
+                while queue!= [] and ref[queue[0]] == 0:
+                    queue.pop(0)
+                    
+                if queue == []:
+                    break
+            
+                temp.append(queue[0])
+                ref[queue[0]] -=1
+
+            if len(temp) == groupSize:
+               
+                temp = []
+                continue
+
+            target = temp[-1] + 1
+            if ref.get(target, 0) == 0 :
+                return False
+                    
+            temp.append(target)
+            ref[target] -=1
+        
+        return True
